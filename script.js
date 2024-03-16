428f594fc0aacfdec1374d29d587c95119b7c3c0
Sarah Kerr <sarah.kerr2@hotmail.com>
2024-03-17 12:44:29 +1300

    Full overhaul with less direct DOM interaction - based on building a house from the inside out https://www.ayweb.dev/blog/building-a-house-from-the-inside-out

--- script.js
+++ script.js
@@ -1,57 +1,63 @@
-// DOM
-const boardGameGrid = document.querySelector(".boardGameGrid");
-
 // Gameboard  object (module)
 //     -store board array inside Gameboard object
-const Gameboard = (() => {
-  let board = [".", ".", ".", ".", ".", ".", ".", ".", "."];
+const Gameboard = () => {
+  const board = [];
+  for (let i = 0; i < 9; i++) {
+    board.push(Cell());
+  }
 
-  // const board = ["O", "O", "X", "O", "X", "O", "O", "O", "X"];
+  const getBoard = () => board;
 
   // update gameboard
-  const updateBoard = (location, token) => {
-    board[location] = token;
+  const placeToken = (location, player) => {
+    // if (board[location] === ".") {
+    console.log("Making a move");
+    console.log(board[location]);
+    board[location].addToken(player);
+    // }
     return;
   };
 
-  const displayBoard = () => {
-    console.log(board);
-    let squareCount = 0;
-    board.forEach(function (element) {
-      console.log(element);
-      const square = document.getElementById(`square-${squareCount}`);
-      console.log(`square-${squareCount}`);
-      const move = document.createElement("p");
-      move.textContent = element;
-      // square.addEventListener("click", (e) => checkMove(e)); TODO 1. needs work on this - DO NEXT
-      square.append(move);
-      squareCount++;
-    });
-    return;
-  };
-  return { displayBoard, updateBoard };
-})();
+  //  we won't need printBoard after we build our UI
+  const printBoard = () => {
+    const boardWithCellValues = board.map((cell) => cell.getValue());
 
-// display gameboard to page
-// IIFE - might change this out when add in a game controller to call functions.
+    console.log(boardWithCellValues);
+  };
+
+  return { getBoard, placeToken, printBoard };
+};
+
+/*
+ ** A Cell represents one "square" on the board and can have one of
+ ** 0: no token is in the square,
+ ** 1: Player One's token,
+ ** 2: Player 2's token
+ */
+function Cell() {
+  let value = ".";
+
+  // Accept a player's token to change the value of the cell
+  const addToken = (player) => {
+    value = player.getToken();
+  };
+
+  // How we will retrieve the current value of this cell through closure
+  const getValue = () => value;
+
+  return {
+    addToken,
+    getValue,
+  };
+}
 
 const Player = (name, token) => {
   const getName = () => name;
   const getToken = () => token;
 
-  // TODO place token logic.
-  const placeToken = (location) => {
-    // TODO dummy location - but will be click event
-    // TODO checkLocation/move is valid
-    // but hardcoded for now
-    // let location = 3;
-    console.log(location);
-    Gameboard.updateBoard(location, getToken());
-    return;
-  };
   //   TODO assign winner e.g player.wins()
 
-  return { placeToken };
+  return { getName, getToken };
 };
 
 // GAME LOGIC
@@ -76,22 +82,97 @@ const Player = (name, token) => {
 //  2 4 6 match
 
 // wins
-// TODO check if token can be placed
 
 // GAME CONTROLLER
 // needs a lot of work
-const gameController = (() => {
+const GameController = () => {
   //  Set up players
   const player1 = Player("P1", "X");
   const player2 = Player("P2", "O");
 
-  // This could be start of game loop
-  // e.g player one -> display -check for winner -> player 2 ->
-  // Dummy play
-  // location is argument
-  player1.placeToken(3);
-  player2.placeToken(4);
+  const board = Gameboard();
 
-  // Display board to screen
-  Gameboard.displayBoard();
-})();
+  let activePlayer = player1;
+
+  const switchPlayerTurn = () => {
+    activePlayer = activePlayer === player1 ? player2 : player1;
+  };
+  const getActivePlayer = () => activePlayer;
+
+  const printNewRound = () => {
+    board.printBoard();
+    console.log(`${getActivePlayer().getName()}'s turn.`);
+  };
+
+  const playRound = (location) => {
+    // Drop a token for the current player
+    console.log(
+      `Placing ${getActivePlayer().getName()}'s token into location ${location}...`
+    );
+    board.placeToken(location, getActivePlayer());
+
+    /*  This is where we would check for a winner and handle that logic,
+        such as a win message. */
+
+    // Switch player turn
+    switchPlayerTurn();
+    printNewRound();
+  };
+
+  // Initial play game message
+  printNewRound();
+
+  // For the console version, we will only use playRound, but we will need
+  // getActivePlayer for the UI version, so I'm revealing it now
+  return {
+    playRound,
+    getActivePlayer,
+    getBoard: board.getBoard,
+  };
+};
+
+const ScreenController = () => {
+  const game = GameController();
+  const playerTurnDiv = document.querySelector(".turn");
+  const boardDiv = document.querySelector(".boardGameGrid");
+
+  const updateScreen = () => {
+    // clear the board
+    boardDiv.textContent = "";
+
+    // get the newest version of the board and player turn
+    const board = game.getBoard();
+    const activePlayer = game.getActivePlayer();
+
+    // Display player's turn
+    playerTurnDiv.textContent = `${activePlayer.getName()}'s turn...`;
+
+    // Render board squares
+    board.forEach((cell, index) => {
+      // Anything clickable should be a button!!
+      const cellButton = document.createElement("button");
+      cellButton.classList.add("cell");
+      // Create a data attribute to identify the cell
+      // This makes it easier to pass into our `playRound` function
+      cellButton.dataset.cell = index;
+      cellButton.textContent = cell.getValue();
+      boardDiv.appendChild(cellButton);
+    });
+  };
+
+  // Add event listener for the board
+  function clickHandlerBoard(e) {
+    const selectedCell = e.target.dataset.cell;
+    // Make sure I've clicked a column and not the gaps in between
+    if (!selectedCell) return;
+
+    game.playRound(selectedCell);
+    updateScreen();
+  }
+  boardDiv.addEventListener("click", clickHandlerBoard);
+
+  // Initial render
+  updateScreen();
+};
+
+ScreenController();
