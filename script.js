94e6dbc9484ab383f44f39563b09f9d8ccc90bd6
Sarah Kerr <sarah.kerr2@hotmail.com>
2024-03-17 13:01:09 +1300

    Remove printBoard and most of the console logs for tidying up script.js

--- script.js
+++ script.js
@@ -11,28 +11,19 @@ const Gameboard = () => {
   // update gameboard
   const placeToken = (location, player) => {
     // if (board[location] === ".") {
-    console.log("Making a move");
-    console.log(board[location]);
     board[location].addToken(player);
     // }
     return;
   };
 
-  //  we won't need printBoard after we build our UI
-  const printBoard = () => {
-    const boardWithCellValues = board.map((cell) => cell.getValue());
-
-    console.log(boardWithCellValues);
-  };
-
-  return { getBoard, placeToken, printBoard };
+  return { getBoard, placeToken };
 };
 
 /*
  ** A Cell represents one "square" on the board and can have one of
- ** 0: no token is in the square,
- ** 1: Player One's token,
- ** 2: Player 2's token
+ ** .: no token is in the square,
+ ** X: Player One's token,
+ ** O: Player 2's token
  */
 function Cell() {
   let value = ".";
@@ -55,36 +46,10 @@ const Player = (name, token) => {
   const getName = () => name;
   const getToken = () => token;
 
-  //   TODO assign winner e.g player.wins()
-
   return { getName, getToken };
 };
 
-// GAME LOGIC
-// TODO check for winner.
-//  0 1 2
-//  3 4 5
-//  6 7 8
-
-// win conditions
-// HORIZONTAL (plus 1 starting at 0, 3, 6)
-//  0 1 2 match
-// 3 4 5 match
-// 6 7 8 match
-
-// VERTICAL (plus 3 starting at 0, 1, 2)
-//  0 3 6 match
-//  1 4 7 match
-//  2 5 8 match
-
-// DIAGONAL (plus 4 starting at 0, plus 2 starting at 2)
-// 0 4 8 match
-//  2 4 6 match
-
-// wins
-
 // GAME CONTROLLER
-// needs a lot of work
 const GameController = () => {
   //  Set up players
   const player1 = Player("P1", "X");
@@ -99,11 +64,6 @@ const GameController = () => {
   };
   const getActivePlayer = () => activePlayer;
 
-  const printNewRound = () => {
-    board.printBoard();
-    console.log(`${getActivePlayer().getName()}'s turn.`);
-  };
-
   const playRound = (location) => {
     // Drop a token for the current player
     console.log(
@@ -114,16 +74,33 @@ const GameController = () => {
     /*  This is where we would check for a winner and handle that logic,
         such as a win message. */
 
+    // GAME LOGIC
+    // TODO check for winner.
+    //  0 1 2
+    //  3 4 5
+    //  6 7 8
+
+    // win conditions
+    // HORIZONTAL (plus 1 starting at 0, 3, 6)
+    //  0 1 2 match
+    // 3 4 5 match
+    // 6 7 8 match
+
+    // VERTICAL (plus 3 starting at 0, 1, 2)
+    //  0 3 6 match
+    //  1 4 7 match
+    //  2 5 8 match
+
+    // DIAGONAL (plus 4 starting at 0, plus 2 starting at 2)
+    // 0 4 8 match
+    //  2 4 6 match
+
+    // wins
+
     // Switch player turn
     switchPlayerTurn();
-    printNewRound();
   };
 
-  // Initial play game message
-  printNewRound();
-
-  // For the console version, we will only use playRound, but we will need
-  // getActivePlayer for the UI version, so I'm revealing it now
   return {
     playRound,
     getActivePlayer,
@@ -163,7 +140,7 @@ const ScreenController = () => {
   // Add event listener for the board
   function clickHandlerBoard(e) {
     const selectedCell = e.target.dataset.cell;
-    // Make sure I've clicked a column and not the gaps in between
+    // Make sure I've clicked a cell and not the gaps in between
     if (!selectedCell) return;
 
     game.playRound(selectedCell);
